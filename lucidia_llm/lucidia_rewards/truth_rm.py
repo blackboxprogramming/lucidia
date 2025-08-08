52d154e8c79fc92de9e2fe40552a37c3bc1f87ae
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-08 01:29:50 -0700

    Add lucidia_rewards/truth_rm.py with truthfulness reward model stub

---
+++ lucidia_llm/lucidia_rewards/truth_rm.py
@@ -0,0 +1,6 @@
+"""Reward model for enforcing truthfulness."""
+
+
+def score(prompt, completion):
+    """Score a completion based on truthfulness."""
+    raise NotImplementedError("Truthfulness reward model not implemented")
