ff0c2579e3d9650684247e44f113d4654668d1d7
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-08 01:36:28 -0700

    Add lucidia_memory/self_embed.py with self-embedding utilities stub

---
+++ lucidia_llm/lucidia_memory/self_embed.py
@@ -0,0 +1,6 @@
+"""Self-embedding utilities for Lucidia."""
+
+
+def embed_text(text):
+    """Compute embedding for a given text."""
+    raise NotImplementedError("Self embedding not implemented")
