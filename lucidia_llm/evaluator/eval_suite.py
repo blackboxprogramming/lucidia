add59f5f76db272b578e3cd4e74168103befcaa2
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-08 01:37:26 -0700

    Add evaluator/eval_suite.py with evaluation suite stub

---
+++ lucidia_llm/evaluator/eval_suite.py
@@ -0,0 +1,6 @@
+"""Evaluation suite for Lucidia models."""
+
+
+def run_all_evaluations(model):
+    """Run a set of evaluation tasks on the given model."""
+    raise NotImplementedError("Evaluation suite not implemented")
