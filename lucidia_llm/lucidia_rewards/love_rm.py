858032df36a6c35ae902f073824159ba864c1eb0
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-08 01:33:47 -0700

    Add lucidia_rewards/love_rm.py with love reward model stub

---
+++ lucidia_llm/lucidia_rewards/love_rm.py
@@ -0,0 +1,6 @@
+"""Reward model for encouraging love and helpfulness."""
+
+
+def score(prompt, completion):
+    """Score a completion based on love/helpfulness."""
+    raise NotImplementedError("Love reward model not implemented")
