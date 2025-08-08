f3bd20ee56fcfa726ebc4d4b0f7735a2e6e41d88
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-08 01:11:56 -0700

    Add lucidia_core/quantize.py with quantization stub

---
+++ lucidia_llm/lucidia_core/quantize.py
@@ -0,0 +1,6 @@
+"""Lucidia model quantization utilities."""
+
+
+def quantize_model(model, bits=8):
+    """Quantize model weights to specified bit width (placeholder)."""
+    raise NotImplementedError("Quantization not implemented")
